8ca21dd09135f03cb96cc8558356ad5a895faf62
Victor Prado <vmpradob@gmail.com>
2022-02-23 16:57:04 -0400

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,45 @@
+var express = require('express');
+const path = require('path');
+const fetch = require('node-fetch');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.sendFile(path.join(__dirname + '/../views/index.html'));
+});
+
+
+router.post("/client-token", async (req, res) => {
+    const url = `${process.env["PRIMER-API-URL"]}/auth/client-token`;
+    const response = await fetch(url, {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json",
+            "X-Api-Key": process.env["PRIMER-API-KEY"],
+        },
+    });
+    const json = await response.json();
+    return res.send(json);
+});
+
+router.post("/authorize", async (req, res) => {
+    const {token} = req.body;
+    const url = `${process.env["PRIMER-API-URL"]}/payments`;
+    const response = await fetch(url, {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json",
+            "X-Api-Key": process.env["PRIMER-API-KEY"],
+            "Idempotency-Key": orderId, // Optionally add an idempotency key
+        },
+        body: JSON.stringify({
+            amount: 700,
+            currencyCode: "EUR",
+            orderId: orderId,
+            paymentInstrument: {
+                token: token,
+            },
+        }),
+    })
+})
+module.exports = router;
